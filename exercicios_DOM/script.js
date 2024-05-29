3955e193d452b91e72add6234f45718529238973
sofialzira <design.sofiaoliveira@gmail.com>
2024-05-29 21:27:42 +0100

    exercicios DOM aulas

--- exercicios_DOM/script.js
+++ exercicios_DOM/script.js
@@ -19,30 +19,84 @@
 // exercicio pseudo eventos 
 
 
-const buttons = document.querySelectorAll("button");
-const buttons2 = document.getElementsByTagName("button");
+// const buttons = document.querySelectorAll("button");
+// const buttons2 = document.getElementsByTagName("button");
 
-console.log(buttons);
-
-
-buttons[0].onmouseover = () => console.log("mouseover");
-buttons[1].onmouseover = () => console.log("mouseover");
+// console.log(buttons);
 
 
-buttons[0].addEventListener("mouseout", () => console.log("mouseout"));
-buttons[1].addEventListener("mouseout", () => console.log("mouseout"));
+// buttons[0].onmouseover = () => console.log("mouseover");
+// buttons[1].onmouseover = () => console.log("mouseover");
 
-buttons.foreach(button => button.onmouseover = () => console.log("mouse over!"));
 
-setTimeout(() => 
-    buttons[0].setAttribute("id", "primeiro botão"), 2000);
+// buttons[0].addEventListener("mouseout", () => console.log("mouseout"));
+// buttons[1].addEventListener("mouseout", () => console.log("mouseout"));
 
-setTimeout(() => buttons[0].setAttribute("id", "teste"), 2000);
+// // buttons.foreach(button => button.onmouseover = () => console.log("mouse over!"));
+
+// setTimeout(() => 
+//     buttons[0].setAttribute("id", "primeiro botão"), 2000);
+
+// setTimeout(() => buttons[0].setAttribute("id", "teste"), 2000);
 
 
 
  
 // objecto evento 
+
+
+// const captureEvent = (event) => {
+//     console.log(event);
+
+//     if (event.type === "click") {
+//         event.target.setAttribute("class", "clicked");
+//   } else if (event.type === "mouseover") {
+//     event.target.setAttribute("class", "hover");
+//   }
+//     };
+
+
+    
+
+// const newButtons = document.querySelectorAll("#btn");
+// console.log(newButtons);
+
+
+// newButtons.forEach((button) => {
+//     button.addEventListener("click", captureEvent);
+//     button.addEventListener("mouseover", captureEvent);
+// });
+
+
+
+
+// exercicio dataform
+
+const form = document.getElementById("formulario");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+const dados = new FormData(form);
+
+const nome = dados.get("nome");
+const email = dados.get("email");
+const mensagem = dados.get("mensagem");
+
+
+console.log(`${nome} - ${email} - ${mensagem}`);
+
+});
+
+
+
+
+
+
+
+
+
+
 
 
 
